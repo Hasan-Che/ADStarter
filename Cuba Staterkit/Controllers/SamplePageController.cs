16fb3a8a5015580bf3bff3c2d8ed24e76654c5e0
Muhammad Hasan Che Abdullah <muhdhasan.chea@gmail.com>
2024-06-16 14:10:10 +0800

    push staterkit
    Adding Staterkit

---
+++ Cuba Staterkit/Controllers/SamplePageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cuba_Staterkit.Controllers
+{
+    public class SamplePageController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
