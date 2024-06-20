6d98aaa95e0c4355669368beba314d4efbe7d980
Nur Khuzairie Zakwan Mohd Zamri <nurkhuzairiezakwan@graduate.utm.my>
2024-06-20 23:23:03 +0800

    Add Repository and delete text.cs files

--- ADStarter.DataAccess/Repository/Repository.cs
+++ ADStarter.DataAccess/Repository/Repository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using ADStarter.DataAccess.Data;
-using ADStarter.DataAccess.Repository.IRepository
+using ADStarter.DataAccess.Repository.IRepository;
 
 
 using Microsoft.EntityFrameworkCore;
