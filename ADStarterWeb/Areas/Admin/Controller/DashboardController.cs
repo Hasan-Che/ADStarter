052afff0f21be1761c2091ef77eaafd4219e15c4
Wan Amirul <wanamirulhafiq@graduate.utm.my>
2024-06-21 15:03:31 +0800

    Add Admin & Customer Service Dashboards
    Add Admin & Customer Service Dashboards

---
+++ ADStarterWeb/Areas/Admin/Controller/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADStarterWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
