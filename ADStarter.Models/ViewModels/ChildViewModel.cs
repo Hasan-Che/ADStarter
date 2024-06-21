631e141cbb5c1da8485c17f97e7084fc61275c72
Nur Khuzairie Zakwan Mohd Zamri <nurkhuzairiezakwan@graduate.utm.my>
2024-06-21 15:16:13 +0800

    Child Registration

---
+++ ADStarter.Models/ViewModels/ChildViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADStarter.Models.ViewModels
+{
+    public class ChildViewModel
+    {
+        public int c_myKid { get; set; }
+        public int prog_ID { get; set; }
+        public int parent_ID { get; set; }
+        public string c_name { get; set; }
+        public int c_age { get; set; }
+        public string c_gender { get; set; }
+        public DateTime c_dob { get; set; }
+        public string c_nationality { get; set; }
+        public string c_religion { get; set; }
+        public string c_race { get; set; }
+        public string c_status { get; set; }
+        public string c_photo { get; set; } // Add this property
+                                            // Add other properties as needed
+    }
+
+}
