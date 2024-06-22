3abf71a04fc72cf4d0fe7d4bad0cd4c3b915de69
Muhammad Izzuddin Bin Shabrin <izzuddin02@graduate.utm.my>
2024-06-22 23:43:32 +0800

    js file

---
+++ ADStarterWeb/wwwroot/js/custom-script.js
@@ -0,0 +1,9 @@
+// you can add your custom script here
+
+// popover
+var exampleEl = document.getElementsByClassName(".example-popover");
+var popover = new bootstrap.Popover(exampleEl, options);
+
+myPopover.show();
+
+
