59134d95cc097b7e762191986e1d0039641c0fb9
Muhammad Izzuddin Bin Shabrin <izzuddin02@graduate.utm.my>
2024-06-26 14:21:56 +0800

    stash

---
+++ ADStarter.Models/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADStarter.Models.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
--- ADStarterWeb/Areas/Admin/Controller/UserListController.cs
+++ ADStarterWeb/Areas/Admin/Controller/UserListController.cs
@@ -1,17 +1,48 @@
-﻿using ADStarter.Utility;
+﻿using ADStarter.Models.ViewModels;
+using ADStarter.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace ADStarterWeb.Areas.Parent.Controllers
+namespace ADStarterWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = SD.Role_Admin)]
     public class UserListController : Controller
     {
-        public IActionResult UserList()
-        {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-            return View();
+        public UserListController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var users = _userManager.Users.ToList();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                if (roles.Contains("Customer Service") || roles.Contains("Therapist"))
+                {
+                    var thisViewModel = new UserRolesViewModel
+                    {
+                        UserId = user.Id,
+                        Email = user.Email,
+                        Roles = roles
+                    };
+                    userRolesViewModel.Add(thisViewModel);
+                }
+            }
+
+            return View(userRolesViewModel);
         }
     }
 }
